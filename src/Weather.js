8eee72965cec4f1e847b1ad49d6099bcd3700f89
lenaMarilena <112854347+lenaMarilena@users.noreply.github.com>
2022-11-03 12:32:33 +0100

    css

--- src/Weather.js
+++ src/Weather.js
@@ -7,13 +7,9 @@ return (
         <form>
             <div className="row">
                 <div className="col-9">
-                
-           
-            <input type="search" placeholder="Enter a city..." className="form-control"/></div>
+            <input type="search" placeholder="Enter a city..." className="form-control" autoFocus="on"/></div>
             <div className="col-3">
-
-           
-            <input type="submit" value="Search" className="btn btn-primary"/> 
+            <input type="submit" value="Search" className="btn btn-primary w-100"/> 
             </div>
              </div>
         </form>
@@ -22,11 +18,11 @@ return (
 <li>Wednesday 7:00</li>
 <li>Mostly Cloudy</li>
         </ul>
-<div className="row">
+<div className="row mt-3">
     <div className="col-6">
         <img src="https://ssl.gstatic.com/onebox/weather/64/partly_cloudy.png" alt="mostly cloudy"
         />
-25°C
+<span className="temperature">25</span><span className="unit">°C</span>
     </div>
     <div className="col-6">
         <ul>
