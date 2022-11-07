5bb3308570b9466504bc9525c1e3dad047f9803f
lenaMarilena <112854347+lenaMarilena@users.noreply.github.com>
2022-11-07 13:30:52 +0100

    add forecast

--- src/FormatDay.js
+++ src/FormatDay.js
@@ -1,29 +1,34 @@
 import React from "react";
+import WeatherIcon from "./WeatherIcon";
 
-export default function FormatDay(props) {
-  let days = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
-  let day = days[props.date.getDay()];
-  let hours = props.date.getHours();
-  if (hours < 10) {
-    hours = `0${hours}`;
-  }
-
-  let minutes = props.date.getMinutes();
-  if (minutes < 10) {
-    minutes = `0${minutes}`;
+export default function ForecasrDay (props) {
+   function maxTemp() {
+      let temperature = Math.round(props.data.temp.max);
+      return `${temperature}°`;
    }
-   
-  return (
-    <div>
-      {day} <br /> {hours}:{minutes}
-    </div>
-  );
+   function minTemp() {
+     let temperature = Math.round(props.data.temp.min);
+     return `${temperature}°`;
+   }
+
+   function day() {
+      let date = new Date(props.data.dt * 1000);
+      let day = date.getDay();
+      let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+      return days[day];
+   }
+   return (
+     <div>
+       <div className="weather-forecast-day">{day()}</div>
+       <WeatherIcon code={props.data.weather[0].icon} size={35} />
+       <div className="weather-forecast-temperatures">
+         <span className="weather-forecast-temperature weather-forecast-temperature-max">
+           {maxTemp()}
+         </span>
+         <span className="weather-forecast-temperature weather-forecast-temperature-min">
+           {minTemp()}
+         </span>
+       </div>
+     </div>
+   );
 }
